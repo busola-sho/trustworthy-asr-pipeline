d63f621787b7b7bf1761c8a1f7d9a717623cdf12
Olubusolami-R <busolasogunle@gmail.com>
2026-05-12 16:40:40 +0100

    feat: implement run_benchmark

---
+++ src/benchmark.py
@@ -0,0 +1,40 @@
+from jiwer import wer
+from src.models import ASRModel
+from src.datasets import Dataset
+import json
+
+
+def run_benchmark(model: ASRModel, dataset: Dataset, output_path: str) -> dict:
+    all_refs=[]
+    all_hyps=[]
+    results=[]
+
+    for sample in dataset.load():
+        transcript=model.transcribe(sample.audio, sample.sample_rate)
+        sample_wer=wer(sample.label.lower(),transcript.text.lower())
+        results.append(
+            {
+                "ref": sample.label,
+                "hyp": transcript.text,
+                "sample_WER": sample_wer,
+            }
+        )
+        all_refs.append(sample.label.lower())
+        all_hyps.append(transcript.text.lower())
+
+    corpus_wer = wer(all_refs,all_hyps)
+    count= len(all_hyps) if len(all_hyps)==len(all_refs) else -1
+
+    output = {
+        "model": model.model_name,
+        "dataset": dataset.name,
+        "corpus_wer": corpus_wer,
+        "num_samples": count,
+        "samples": results
+    }
+
+    with open(output_path, "w") as f:
+        json.dump(output, f, indent=2)
+    
+    return {"model":model.model_name, "dataset":dataset.name, "num_of_samples":count, "WER":corpus_wer}
+
